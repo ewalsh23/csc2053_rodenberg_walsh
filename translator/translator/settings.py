a55bb40ffd6df56db0d315f709260e7230d8b94d
ewalsh23 <ewalsh25@villanova.edu>
2025-04-27 15:55:30 -0400

    fixed settings.py to use the correct path

--- translator/translator/settings.py
+++ translator/translator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*52vtnid!^*-198s-(j)ej%2q4xb7x#pkd5m267594q5+_3+x!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -116,7 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
